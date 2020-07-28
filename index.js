75809afae793efa193d5907dd8d2779e3aa2e6a0
Ian McElroy <mcelroyian@gmail.com>
2020-07-28 18:00:48 -0500

    wip

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const width = 800;
-const height = 800;
+const width = 600;
+const height = 600;
 const box = 10; 
 const size = width / box;
 var canvas = document.getElementById("conway");
@@ -7,18 +7,19 @@ canvas.width = width;
 canvas.height = height;
 var ctx = canvas.getContext("2d");
 generation = 0
+let gameloop = null;
+let genDisplay = document.getElementById("generation")
 
 
 function draw(grid) {
+    console.log("drawn")
     for (var i = 0; i < size; i++) {
         for (var j = 0; j < size; j++) {
-            if (grid[i][j]) {
-                ctx.fillStyle = 'blue'
+                ctx.fillStyle = grid[i][j] ? "black" : "white"
                 ctx.beginPath();
                 ctx.rect(j * box, i * box, box, box );
                 ctx.stroke();
                 ctx.fill();
-            }
         }
     }
 }
@@ -34,11 +35,30 @@ function createGrid() {
     return grid
 }
 
+canvas.addEventListener('mousemove', e => {
+    let mousePos = getMousePos(e)
+    console.log(mousePos)
+})
+
+canvas.addEventListener('click', e => {
+    console.log("hello")
+    let box = getMousePos(e);
+    let newValue = grid[box.y][box.x] == 1 ? 0 : 1;
+    grid[box.y][box.x] = newValue;
+    console.log(newValue);
+    draw(grid)
+})
+
 let grid = createGrid();
-draw(grid)
+ draw(grid)
 
-requestAnimationFrame(tick)
-
+function getMousePos(e) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+        x: Math.floor((e.clientX - rect.left) / box),
+        y: Math.floor((e.clientY - rect.top) / box)
+    };
+}
 
 function nextGrid(grid) {
     const nextGrid = grid.map(arr => [...arr])
@@ -46,8 +66,8 @@ function nextGrid(grid) {
     for (var i = 0; i < size; i++) {
         for (var j = 0; j < size; j++) {
             let cell =  grid[i][j]
+            let starting = cell
             let count = countNeighbors(grid, i, j)
-            console.log(i,j, "C ", count)
             if (cell == 1 && count < 2) {
                 cell = 0
             } else if (cell == 1 && count > 3) {
@@ -56,17 +76,39 @@ function nextGrid(grid) {
                 cell = 1
             }
             nextGrid[i][j] = cell
+            //console.log(starting, count, cell)
         }
     }
     return nextGrid
+}
+
+function startLoop() {
+    gameloop = requestAnimationFrame(tick)
+}
+
+function pauseLoop() {
+    console.log("PAUSE")
+    cancelAnimationFrame(gameloop)
+    gameloop = null;
+}
+
+function newStart() {
+    generation = 0
+    genDisplay.innerHTML = generation
+    grid = createGrid()
+    draw(grid)
 }
 
 function tick() {
     grid = nextGrid(grid)
     draw(grid)
     generation++
-    console.log(generation)
-    requestAnimationFrame(tick)
+    genDisplay.innerHTML = generation
+    //console.log(generation)
+    if (gameloop){
+        requestAnimationFrame(tick)
+    }
+    
 }
 
 function countNeighbors(grid, col, row) {
@@ -79,10 +121,9 @@ function countNeighbors(grid, col, row) {
             x = col + n >= 0 ? ( col + n <= size -1 ? col + n : 0): size - 1
             y = row + m >= 0 ? ( row + m <= size -1 ? row + m : 0): size - 1            
         count += grid[x][y]
-        console.log("Added", x, y)
         }
     }
     return count
 }
 // console.log(grid)
-console.log(nextGrid(grid))
+//console.log(nextGrid(grid))
